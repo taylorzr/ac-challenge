17db4fbf2f2d01b3a2860bbb490b6ce78b26bdbe
Zach Taylor <taylorzr@gmail.com>
2014-12-18 02:57:33 -0600

    Slight refactor

--- qa_test/test.rb
+++ qa_test/test.rb
@@ -8,17 +8,20 @@ Capybara.current_driver = :selenium
 Capybara.app_host = 'http://demo.avantcredit.com'
 
 feature "Demo" do 
-
   include DemoHelper
 
-  scenario "Filling out application, and signing agreement" do
+  background do
     retrieve_application
     fill_out_application
     accept_credit_score
     select_ach
     fill_out_contract
     sleep(1) until (page.has_content?("Continue to Final Step"))
-    expect(page).to have_content "Continue to Final Step"
+  end
+
+  scenario "Filling out application, and signing agreement" do
+    expect(page).to have_content "Account Type"
+    expect(page).to have_content "Bank Account"
   end
 end
 
